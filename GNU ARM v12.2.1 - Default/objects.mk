6b6c19d4e4693cd0be1a13a3da7042a00bfe2af3
trungnguyen0503 <86910893+trungnguyen0503@users.noreply.github.com>
2024-09-04 23:38:13 +0700

    Final source code

---
+++ GNU ARM v12.2.1 - Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Home/Apps/SiliconLabs/SimplicityStudio/sdk/gecko_sdk//platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a C:/Home/Apps/SiliconLabs/SimplicityStudio/sdk/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg28_gcc_release.a C:/Home/Apps/SiliconLabs/SimplicityStudio/sdk/gecko_sdk//protocol/z-wave/ZWave/lib/libZWaveSlave_800s.a C:/Home/Apps/SiliconLabs/SimplicityStudio/sdk/gecko_sdk//protocol/z-wave/platform/SiliconLabs/PAL/lib/libzpal_efr32zg28b312f1024im48.a
+
+LIBS := -lgcc -lc -lm -lnosys
+
